018a0fd4e04ce5973d6038f6118c6f12a1550349
hassangames <hassangames1998@gmail.com>
2021-02-10 21:08:35 +0200

    Documents

--- TelegramBot/TelegramSender.py
+++ TelegramBot/TelegramSender.py
@@ -3,6 +3,7 @@ from telegram_send import telegram
 
 
 def SendToChannel(message):
+    ":argument: message: a text of the news"
     try:
         telegram_send.send(parse_mode=telegram.ParseMode.MARKDOWN, messages=[message])
     except BaseException as e:
