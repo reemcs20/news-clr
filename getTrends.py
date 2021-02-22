d15531117db86a424496f48a70494e7f2e92541b
hassangames <hassangames1998@gmail.com>
2021-02-22 16:39:59 +0200

    testing JSON class

--- getTrends.py
+++ getTrends.py
@@ -2,7 +2,7 @@ import argparse
 
 from core.Trends.TrendPoster import AJ_Trends, BBC_Trends
 import json
-
+import sys
 args = argparse.ArgumentParser()
 args.add_argument('-p', default=None, help="Prettify JSON", type=str, required=False)
 args_parser = args.parse_args()
@@ -20,7 +20,7 @@ def PringJson():
         for news in t.AllTrends.get("aljazeera"):
             print(news.get("title"))
     else:
-        ...
+        sys.stdout.write(json.dumps(t.AllTrends, indent=3, sort_keys=True))
 
 
-print(json.dumps(t.AllTrends, indent=3, sort_keys=True))
+PringJson()
