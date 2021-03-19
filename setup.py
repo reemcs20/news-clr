50965e533ae4af5533b01859e705cbac95ddf101
hassangames <hassangames1998@gmail.com>
2021-03-19 17:31:40 +0200

    3/19/2021 Release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NewsCrawlers',
+    version='1.0.0',
+    packages=['core'],
+    url='',
+    license='',
+    author='Hassan',
+    author_email='halsmiri@ucas.edu.ps',
+    description='news crawler'
+)
